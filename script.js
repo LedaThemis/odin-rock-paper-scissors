9a9eaab2239148260fbe79fed7826dbd4cfaa65d
Leda <ledathemis@protonmail.com>
2022-03-05 10:45:22 +0300

    add playRound function

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ function computerPlay() {
   const randomIndex = Math.floor(Math.random() * 3);
   return choices[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+  // To make function case-insensitive
+  const pS = playerSelection.toLowerCase();
+  const cS = computerSelection.toLowerCase();
+  if (pS === cS) {
+    return "It's a tie!";
+  } else if (
+    (pS === "rock" && cS === "scissors") ||
+    (pS === "paper" && cS === "rock") ||
+    (pS === "scissors" && cS === "paper")
+  ) {
+    return "Player wins!";
+  } else {
+    return "Computer wins!";
+  }
+}
+
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection));
