9e5fdd1982225894a2a0be1262f4d90a6976480b
Leda <ledathemis@protonmail.com>
2022-03-05 10:34:59 +0300

    add computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("TEST");
+function computerPlay() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
+}
